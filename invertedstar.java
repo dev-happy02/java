4d03385e229f6a08829eabddd633180d571561cb
{Happy <happyswaraj7667@gmail.com>
2025-06-24 18:27:39 +0530

    inverted stars

---
+++ invertedstar.java
@@ -0,0 +1,29 @@
+/*
+
+
+output
+
+* * * * 
+* * * 
+* * 
+* 
+
+
+ */
+
+import java.util.Scanner;
+
+public class invertedstar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter the number :");
+        int num = sc.nextInt();
+        for (int i=1; i<=num; i++) {
+            for (int j=1; j<=num-i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println( );
+        }
+        sc.close();
+    }
+}
