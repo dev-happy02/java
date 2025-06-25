ad9c2e875eeb546a20152000c2c2e785519cc33e
{Happy <happyswaraj7667@gmail.com>
2025-06-25 09:59:57 +0530

    half pyramid pattern

--- halfpyramid.java
+++ halfpyramid.java
@@ -1,26 +1,26 @@
 /*
+printing half pyramid pattern
+output 
 
-a
-output
-
-* * * * 
-* * * 
-* * 
-* 
-
+1
+12
+123
+1234
+12345
 
  */
 
+
 import java.util.Scanner;
 
-public class invertedstar {
+public class halfpyramid {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("enter the number :");
         int num = sc.nextInt();
-        for (int i=1; i<=num; i++) {
-            for (int j=1; j<=num-i; j++) {
-                System.out.print("* ");
+        for (int i = 1; i <=num; i++) {
+            for (int j = 1; j <=i; j++) {
+                System.out.print(j);
             }
             System.out.println( );
         }
