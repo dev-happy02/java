b2504c9e5d5759c5ad2ac6e0b94eec4af65f0cad
{Happy <happyswaraj7667@gmail.com>
2025-06-26 16:23:43 +0530

    update

--- funtmethod.java
+++ funtmethod.java
@@ -1,9 +1,58 @@
-public class funtmethod {
-    public static void printhello(){
-        System.out.println("hello world :");
-        return;
+// type:- 01.
+// public class funtmethod 
+//     {
+//     public static void printhello(){
+//         System.out.println("hello world :");
+//         return;
+//     }
+//     public static void main(String[] args) {
+//         printhello();
+//     }
+// }
+
+
+
+// type:- 02.
+
+// import java.util.Scanner;
+//     public class funtmethod 
+// {
+//     public static int calculatesum( ) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("enter first number :");
+//         int a = sc.nextInt();
+//         System.out.print("enter second number :");
+//         int b = sc.nextInt();
+//         int sum = a+b;
+//         System.out.println("sum is "+ sum );
+//         return sum;
+//     }
+//     public static void main(String[] args) {
+//         calculatesum( );
+//     }
+// }
+
+
+
+// type:- 03.
+
+
+import java.util.Scanner;
+    public class funtmethod 
+{
+    public static int calculatesum(int a, int b ) {
+        int sum = a+b;
+        System.out.println("sum is "+ sum );
+        return sum;
     }
     public static void main(String[] args) {
-        printhello();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter first number :");
+        int a = sc.nextInt();
+        System.out.print("enter second number :");
+        int b = sc.nextInt();
+        calculatesum(a, b );
     }
 }
+
+
